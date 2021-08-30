bb8a4bfa6a8854b2eed406b10a625e4430b7fa11
Michał Długosz <mdugosz@gmail.com>
2021-08-30 23:01:39 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Main from './components/Main';
+import Shop from './components/Shop';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 function App() {
-  return null;
+  const fetchData = async () => {
+    const data = await fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((value) => console.log(value));
+  };
+  return (
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

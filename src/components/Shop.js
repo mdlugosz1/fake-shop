cdf63dcdd06ab17c5f23a126f45c7ad9159db4a5
Michał Długosz <mdugosz@gmail.com>
2021-08-30 23:01:21 +0200

    Add placeholder

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+const Shop = () => {
+  return (
+    <section>
+      <div className="container">
+        <h1>Item</h1>
+        <h1>Item</h1>
+        <h1>Item</h1>
+        <h1>Item</h1>
+        <h1>Item</h1>
+        <h1>Item</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Shop;

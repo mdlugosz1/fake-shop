b3780ae5b8b317ade36b52749d92ac644c8a9af0
Michał Długosz <mdugosz@gmail.com>
2021-09-01 16:01:50 +0200

    Add cart

---
+++ src/components/Cart.js
@@ -0,0 +1,16 @@
+const Cart = () => {
+  return (
+    <div>
+      <div>
+        <img alt="product" />
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+      </div>
+      <h3>Total Price:</h3>
+      <h3>0.00</h3>
+      <button>Checkout</button>
+      <button>Close</button>
+    </div>
+  );
+};

30269abc871e7dc527b91a73a314d1cff7bd565c
Michał Długosz <mdugosz@gmail.com>
2021-09-01 16:03:06 +0200

    Add API data for product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,17 +1,37 @@
 import Button from './Button';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
   return (
-    <div>
+    <Card>
       <div>
-        <img alt="product" />
-        <p>Some kind of product desciption</p>
+        <Link to={`/shop/${props.id}`}>
+          <ProductImage alt="product" src={props.image} />
+        </Link>
+        {/* <p>{props.description}</p> */}
       </div>
-      <h4>Product Name</h4>
-      <h5>Price</h5>
+      <h4>{props.title}</h4>
+      <h4>{props.price}$</h4>
       <Button text="Add to cart" />
-    </div>
+    </Card>
   );
 };
 
+const ProductImage = styled.img`
+  height: 100px;
+  width: 100px;
+`;
+
+const Card = styled.div`
+  animation: 2.5s ease 0s 1 normal none running fadeIn;
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
 export default ProductCard;

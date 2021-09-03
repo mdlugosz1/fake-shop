cfbf1c004699ecfb8136206770da31b964c10acf
Michał Długosz <mdugosz@gmail.com>
2021-09-03 19:01:38 +0200

    Add methods in CartProviedr to add items and sum them

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -3,9 +3,37 @@ import { createContext, useState } from 'react';
 const CartContext = createContext();
 
 const CartProvider = (props) => {
-  const [quantity, setQuantity] = useState(0);
+  const [boughtItemList, setBoughtItemList] = useState([]);
+
+  const addItem = (addedItem) => {
+    const findItem = boughtItemList.find((item) => item.addedItem.id === addedItem.id);
+
+    //Add quantity key while adding product if it isn't on the list, increase it by one if it is.
+    if (!findItem) {
+      setBoughtItemList([...boughtItemList, { addedItem, quantity: 1 }]);
+    } else {
+      setBoughtItemList((prevState) => {
+        findItem.quantity += 1;
+        return [...prevState];
+      });
+    }
+  };
+
+  //Used to show number on cart icon
+  const sumItemQuantity = () => {
+    let sum = 0;
+
+    boughtItemList.forEach((item) => {
+      sum += item.quantity;
+    });
+
+    return sum;
+  };
+
   return (
-    <CartContext.Provider value={[quantity, setQuantity]}>{props.children}</CartContext.Provider>
+    <CartContext.Provider value={{ addItem, sumItemQuantity, boughtItemList }}>
+      {props.children}
+    </CartContext.Provider>
   );
 };
 

847c231ed54007d8cdea9e77970ed17dfa8866d4
Michał Długosz <mdugosz@gmail.com>
2021-09-03 19:02:43 +0200

    Add styles import to index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/reset.css';
+import './styles/style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));

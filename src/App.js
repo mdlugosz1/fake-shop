460de0e7ecccfbc6c4149c67b60f9f434ec61543
Michał Długosz <mdugosz@gmail.com>
2021-09-03 19:03:11 +0200

    Add CarProvider to use context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import Main from './components/Main';
 import Shop from './components/Shop';
 import Navigation from './components/Navigation';
+import ProductPage from './components/ProductPage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { CartProvider } from './components/CartContext';
 
 function App() {
-  const fetchData = async () => {
-    const data = await fetch('https://fakestoreapi.com/products')
-      .then((response) => response.json())
-      .then((value) => console.log(value));
-  };
   return (
     <Router>
-      <Navigation />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/shop" component={Shop} />
-      </Switch>
+      <CartProvider>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/shop/:id" component={ProductPage} />
+        </Switch>
+      </CartProvider>
     </Router>
   );
 }

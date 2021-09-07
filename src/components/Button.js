79657e52b0d89ca30401235611dbb436f58352d0
Michał Długosz <mdugosz@gmail.com>
2021-09-07 21:32:26 +0200

    Add button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,11 @@ const Button = (props) => {
   return <MyButton onClick={props.action}>{props.text}</MyButton>;
 };
 
-const MyButton = styled.button``;
+const MyButton = styled.button`
+  border: none;
+  background-color: #f3c93e;
+  padding: 10px 45px;
+  cursor: pointer;
+`;
 
 export default Button;

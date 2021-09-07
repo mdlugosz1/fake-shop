2e763659664d24f89ebbd981213dbc3824345d69
Michał Długosz <mdugosz@gmail.com>
2021-09-07 21:32:36 +0200

    Add cart styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import { CartContext } from './CartContext';
 import Checkout from './Checkout';
+import styled from 'styled-components';
 
 const Cart = () => {
   const [isClicked, setIsClicked] = useState(false);
@@ -8,12 +9,34 @@ const Cart = () => {
   const { sumItemQuantity } = useContext(CartContext);
 
   return (
-    <div onClick={() => setIsClicked(!isClicked)}>
-      <i className="bi bi-cart3"></i>
-      {sumItemQuantity() > 0 && <span>{sumItemQuantity()}</span>}
+    <NavCart className>
+      <i className="bi bi-cart3" onClick={() => setIsClicked(!isClicked)}></i>
+      {sumItemQuantity() > 0 && <CartCounter>{sumItemQuantity()}</CartCounter>}
       {isClicked && <Checkout />}
-    </div>
+    </NavCart>
   );
 };
+
+const NavCart = styled.div`
+  position: relative;
+`;
+
+const CartCounter = styled.span`
+  position: absolute;
+  display: inline-block;
+  min-width: 10px;
+  padding: 3px 3px;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 1;
+  color: #3a4468;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  background-color: #f3c93e;
+  border: 2px solid #f3c93e;
+  border-radius: 10px;
+  margin-top: -15px;
+`;
 
 export default Cart;
